3919cc238f86d5fa89d667a83ef3ecabcae2da50
pilarleminski <pilar.leminski@gmail.com>
2022-05-19 16:58:56 -0300

    Update n1-exe01.py

--- n1-exe01.py
+++ n1-exe01.py
@@ -1,5 +1,7 @@
 '''
-Elabore um programa que efetue a leitura de valores positivos reais até que 0 ou um valor negativo seja fornecido. Aplique o tratamento de exceção para recusar dados inválidos. Ao final devem ser apresentados: o maior e o menor valores informados pelo usuário, a quantidade de valores aceitos pelo programa, a soma e a média dos números lidos.
+Elabore um programa que efetue a leitura de valores positivos reais até que 0 ou um valor negativo seja fornecido. 
+Aplique o tratamento de exceção para recusar dados inválidos. Ao final devem ser apresentados: o maior e o menor 
+valores informados pelo usuário, a quantidade de valores aceitos pelo programa, a soma e a média dos números lidos.
 '''
 
 count=0
